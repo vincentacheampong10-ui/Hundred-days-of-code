4c4f6bd8da1fbbf12b5d8668207ba1d325ec251f
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-04-27 16:16:35 -0400

    Working-with-variable

---
+++ DAY-1-Beg-Working-with-variables .py
@@ -0,0 +1,28 @@
+
+
+##-- print("hello world\nI am learning python.")
+
+##-- print("Hello " + "Angela")
+
+## This line of code will take an input using the input() function
+#-- print("Hello " + input("What is your name?: ") +"!")
+
+##-- username = input("What is your name?: ")
+##-- print("Hello "+ username +"!")
+
+#Fiqure out how many characters in the name typed.
+##-- print(len(username))
+
+
+# We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice.
+# Write 3 lines of code to switch the contents of the variables.
+# You are not allowed to type the words "milk" or "juice".
+# You are only allowed to use variables to solve this exercise.
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass1 = "juice"
+glass2 = "milk"
+
+print(glass1 +" " +glass2)

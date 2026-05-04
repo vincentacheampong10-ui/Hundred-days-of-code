975d3da90ffe60d6738778930b5aa28655bc8424
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-04 09:55:00 -0400

    Mathematical Operations in Python

--- DAY-2-BEG-Understanding-Data-Types.py
+++ DAY-2-BEG-Understanding-Data-Types.py
@@ -1,1 +1,8 @@
-print("Number of letters in your name: " + len(input("Enter your name: ")))
+print(123 + 123)
+print(7 - 3)
+print(3 * 2)
+print(5 / 3)
+print(5 // 3) # Used to get rid of decimals after division
+print(2 ** 2) #Double ** is used for to the power example 2to the power of 2 = 2**2
+
+print(3 * 3 + 3 / 3 - 3)

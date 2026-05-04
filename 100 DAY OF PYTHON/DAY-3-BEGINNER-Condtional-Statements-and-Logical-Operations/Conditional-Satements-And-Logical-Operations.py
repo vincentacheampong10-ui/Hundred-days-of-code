799255833a424960191d4a6382f5647a30ae9f26
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-04 11:48:50 -0400

    multiple if/elif/else statements added

--- 100 DAY OF PYTHON/DAY-3-BEGINNER-Condtional-Statements-and-Logical-Operations/Conditional-Satements-And-Logical-Operations.py
+++ 100 DAY OF PYTHON/DAY-3-BEGINNER-Condtional-Statements-and-Logical-Operations/Conditional-Satements-And-Logical-Operations.py
@@ -2,15 +2,24 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm?:"))
 
 if height >= 120:
-    print("You can ride the rollercoaster")
+    # print("You can ride the rollercoaster")
     age = int(input("What is your age?:"))
     if age <= 6:
         print("Not allowed on rollercoaster")
     elif age <= 17:
-        print("Please pay $17")
+        bill = 17
+        print("Youth tickets are 17")
     else:
-        print("Please pay $27")
+        bill = 27
+        print("Adult tickets are 27")
 
+    wants_photo = input("Do you want to have your photo taken? Type y for yes and n for No.")
+    if wants_photo == "y":
+        #Add $3 to the bill
+        print(f"The payment is now ${bill + 3} ")
+
+    if wants_photo == "n":
+        print(f"The payment is ${bill}")
 
 else:
     print("Sorry you have to grow taller to ride.")

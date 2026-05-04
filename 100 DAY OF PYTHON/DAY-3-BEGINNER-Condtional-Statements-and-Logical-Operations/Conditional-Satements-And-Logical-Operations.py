6242b4bb557023be10a9eb8222af220c354a7da9
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-04 11:23:54 -0400

    Nested if statements

--- 100 DAY OF PYTHON/DAY-3-BEGINNER-Condtional-Statements-and-Logical-Operations/Conditional-Satements-And-Logical-Operations.py
+++ 100 DAY OF PYTHON/DAY-3-BEGINNER-Condtional-Statements-and-Logical-Operations/Conditional-Satements-And-Logical-Operations.py
@@ -3,5 +3,14 @@ height = int(input("What is your height in cm?:"))
 
 if height >= 120:
     print("You can ride the rollercoaster")
+    age = int(input("What is your age?:"))
+    if age <= 6:
+        print("Not allowed on rollercoaster")
+    elif age <= 17:
+        print("Please pay $17")
+    else:
+        print("Please pay $27")
+
+
 else:
     print("Sorry you have to grow taller to ride.")

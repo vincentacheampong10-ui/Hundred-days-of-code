1d21d66d274b6aa9f712be5d94144e76b96fb5d4
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-04 09:58:36 -0400

    BMI Calculator exercise

--- DAY-2-BEG-Understanding-Data-Types.py
+++ DAY-2-BEG-Understanding-Data-Types.py
@@ -1,8 +1,8 @@
-print(123 + 123)
-print(7 - 3)
-print(3 * 2)
-print(5 / 3)
-print(5 // 3) # Used to get rid of decimals after division
-print(2 ** 2) #Double ** is used for to the power example 2to the power of 2 = 2**2
+height = 1.65
+weight = 84
 
-print(3 * 3 + 3 / 3 - 3)
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(bmi)

cb1cf6d04a63e5ae71c8b11fea08929c92ce248b
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-05 10:41:29 -0400

    random module

---
+++ 100 DAY OF PYTHON/DAY-4-BIGINNER-Randomisation-and-Python-Lists/random_module.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+
+random_integer = random.randint(1,10)
+print(random_integer)
+
+# print(my_module.my_favourite_number)
+random_number_0_to_1 = random.random() * 10
+print(random_number_0_to_1)
+
+random_float = random.uniform(1,10)
+print(random_float)

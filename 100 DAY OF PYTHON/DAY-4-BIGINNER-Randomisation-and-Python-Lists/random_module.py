7fffb6ba8d1da69080a962b78a015757e11673c3
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-05 10:46:25 -0400

    heads or tail

--- 100 DAY OF PYTHON/DAY-4-BIGINNER-Randomisation-and-Python-Lists/random_module.py
+++ 100 DAY OF PYTHON/DAY-4-BIGINNER-Randomisation-and-Python-Lists/random_module.py
@@ -1,12 +1,20 @@
 import random
 import my_module
 
-random_integer = random.randint(1,10)
-print(random_integer)
+# random_integer = random.randint(1,10)
+# print(random_integer)
+#
+# # print(my_module.my_favourite_number)
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+#
+# random_float = random.uniform(1,10)
+# print(random_float)
 
-# print(my_module.my_favourite_number)
-random_number_0_to_1 = random.random() * 10
-print(random_number_0_to_1)
+random_heads_or_tails = random.randint(0, 1)
+# print(random_integer)
 
-random_float = random.uniform(1,10)
-print(random_float)
+if random_heads_or_tails == 1:
+    print("Heads")
+else:
+    print("Tails")

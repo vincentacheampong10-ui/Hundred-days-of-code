f83d1b5f43ebeaa9d9eda858f6d510fff47bdfe0
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-05 11:05:23 -0400

    appending to list

---
+++ 100 DAY OF PYTHON/DAY-4-BIGINNER-Randomisation-and-Python-Lists/appending_to_lists.py
@@ -0,0 +1,11 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia","Connecticut", "Massachusetts"]
+# print(states_of_america[0])
+
+states_of_america[1] = "Penncilvania"
+# print(states_of_america[1])
+
+states_of_america.append("Maryland")
+# print(states_of_america[-1])
+
+states_of_america.extend(["South Carolina", "New Hampshire", "Virginia", "New York"])
+print(states_of_america)

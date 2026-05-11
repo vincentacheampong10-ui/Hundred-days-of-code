f79b6bb9b35e297cfcce5a6c58ffed3daf9a74ba
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-11 11:52:30 -0400

    functions with inputs

---
+++ 100 DAY OF PYTHON/DAY-9-BIGINNER-Working-with-Variables-to-Manage-Data/Dictionaries.py
@@ -0,0 +1,24 @@
+programming_dictionary = {
+    "Bug": "An error is a program that prevents the program from working correctly",
+     "Function": "A piece of code that you can easily call over and over again",
+    "Loop": "The action of doing something over and over again"
+}
+
+
+print(programming_dictionary["Bug"])
+
+# Add to an existing dictionary
+programming_dictionary["loops"] = "The action of doing something over and over again"
+print(programming_dictionary)
+
+# Wipe an existing dictionary
+programming_dictionary = {}
+print(programming_dictionary)
+
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer"
+print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])

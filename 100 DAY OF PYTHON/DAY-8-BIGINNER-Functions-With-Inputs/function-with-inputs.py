41a66e5cc42ff4a19e2f12d6c7b9aaecc0053d7d
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-11 11:31:19 -0400

    functions with inputs

---
+++ 100 DAY OF PYTHON/DAY-8-BIGINNER-Functions-With-Inputs/function-with-inputs.py
@@ -0,0 +1,22 @@
+# def greet():
+#     print("Hello")
+#     print("How are you doing?:")
+#     print("Isn't the weather nice?")
+#
+#
+#
+# # Functions that allow for input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you doing {name}?:")
+#     print(f"Isn't the weather nice? {name}")
+#
+# greet_with_name("Vini")
+
+def greet_with(name,  location):
+    print(f"Hello {name}")
+    print(f"Whats is it like in {location}")
+
+greet_with("Vini", "Hungary")
+greet_with(location="Hungary" , name="Vini")

3f151f6b2ebb2a78e222f57b2bf1e325f627fadc
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-12 10:06:20 -0400

    local and global scope

---
+++ 100 DAY OF PYTHON/DAY-1-TO-DAY-10/DAY-12-Scope-and-Number-Guessing-Game/scope.py
@@ -0,0 +1,21 @@
+enemies = 1
+
+
+
+def increase_enemies():
+    # Local Scope
+    enemies = 2
+    print(f"enimes inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+# Global Scope
+player_health = 10
+
+def drink_potion():
+    potion_strength = 2
+    print(player_health)
+
+drink_potion()
+player_health

b6e4d5d9aada93073d1c9f84d3a94c30aa37d25a
Vincentacheampong <Vincentacheampong10@gmail.com>
2026-05-12 09:47:34 -0400

    inputs functions with Outputs

--- 100 DAY OF PYTHON/DAY-1-TO-DAY-10/DAY-10-BIGINNER-Functions-With-Outputs/functions_with_output.py
+++ 100 DAY OF PYTHON/DAY-1-TO-DAY-10/DAY-10-BIGINNER-Functions-With-Outputs/functions_with_output.py
@@ -1,9 +1,14 @@
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs "
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
+
     # print(formated_f_name, formated_l_name)
+
+print(format_name(input("What is your first name:"), input("What is your last name:")))
 
 formated_string = format_name("vini", "ebeate")
 print(formated_string)
